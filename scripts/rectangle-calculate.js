c32ca559074945813852ca7620ba72b321636e9b
arif-05 <arifcse949@gmail.com>
2024-09-24 19:52:47 +0600

    structure done

--- scripts/rectangle-calculate.js
+++ scripts/rectangle-calculate.js
@@ -1,4 +1,19 @@
 
 function calculateRectangleArea(){
-    
+    // length
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+    // width
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+
+    //area
+    const area = length * width;
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
